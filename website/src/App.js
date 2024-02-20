eaf58f929c07e7466a3c029e3285e29d033f8c6d
WONG <devon@lnxsrv11.seas.ucla.edu>
2024-02-19 20:08:31 -0800

    fetched and displayed posters from discover section

---
+++ website/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Fetch from './Fetch'
+
+function App(){
+  return(
+
+    <html>
+      
+      <body>
+        <h1 align="middle">placeholder text</h1>
+      </body>
+
+      <div>
+        <Fetch/>
+      </div>
+    
+  </html>
+    
+  )
+}
+
+export default App
