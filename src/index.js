57ac3346c63f9c440afec53df349c9954da0b492
AliCodeMxzzi <alimoazzami2004@gmail.com>
2024-02-26 02:50:22 -0800

    User Sign In Page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './Sign.css';
+
 import App from './App';
+import Sign from './Sign';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Sign />
   </React.StrictMode>
 );
