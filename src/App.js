3ebbb18a0fb3a6e4582d93d1d3f458b60bdd82e9
BingHongNi <nibinghong826@gmail.com>
2024-02-26 02:32:51 -0800

    add login page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Fetch from './Fetch';
-import MovieDetail from './MovieDetail'; // Assume this is your new page component
+import MovieDetail from './MovieDetail';
+import Login from './login';
+import Home from './home';
+import './App.css'
 
-function App(){
-  return(
-  <Router>
-  <Routes>
-    <Route path="/" element={<Fetch />} />
-    <Route path="/movie/:id" element={<MovieDetail />} />
-  </Routes>
-</Router>
-  )
+function App() {
+
+  const [email, setEmail] = useState('');
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home email={email} loggedIn={loggedIn} setLoggedIn={setLoggedIn} />} />
+          <Route path="/login" element={<Login setLoggedIn={setLoggedIn} setEmail={setEmail} />} />
+          <Route path="/fetch" element={<Fetch />} />
+          <Route path="/movie/:id" element={<MovieDetail />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/home.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const Home = (props) => {
+  const { loggedIn, email } = props
+  const navigate = useNavigate()
+
+  const onButtonClick = () => {
+    if (loggedIn) {
+        // 如果已经登录，这里可以添加登出的逻辑
+        // 例如：setLoggedIn(false) 和清除用户状态
+      } else {
+        // 如果未登录，导航到登录页面
+        navigate('/login');
+      }
+  }
+
+  return (
+    <div className="mainContainer">
+      <div className={'titleContainer'}>
+        <div>Welcome!</div>
+      </div>
+      <div>This is the home page.</div>
+      <div className={'buttonContainer'}>
+        <input
+          className={'inputButton'}
+          type="button"
+          onClick={onButtonClick}
+          value={loggedIn ? 'Log out' : 'Log in'}
+        />
+        {loggedIn ? <div>Your email address is {email}</div> : <div />}
+      </div>
+    </div>
+  )
+}
+
+export default Home
