3a72fa591fe794d04d5119a48c4e0b9219524f23
Kevin Nguyen <kevinvietnguyen2003@gmail.com>
2024-03-13 15:00:48 -0700

    Added more css and animation to privateWrapped

--- src/components/PrivateWrappedMovies.jsx
+++ src/components/PrivateWrappedMovies.jsx
@@ -1,7 +1,28 @@
-
-import React from 'react';
+import React, { useRef } from 'react';
+import { motion, useScroll, useTransform } from 'framer-motion';
 
 export const Movies = ({ baseUrl, topFiveMovies }) => {
+  const lightsRef = useRef(null);
+  const cameraRef = useRef(null);
+  const actionRef = useRef(null);
+  const { scrollYProgress: lightsProgress } = useScroll({
+    target: lightsRef,
+    offset: [1, 0.7]
+  });
+  const { scrollYProgress: cameraProgress  } = useScroll({
+    target: cameraRef,
+    offset: [1, 0.7]
+  });
+  const { scrollYProgress: actionProgress } = useScroll({
+    target: actionRef,
+    offset: [1, 0.7]
+  });
+  const lightsScale = useTransform(lightsProgress, [0, 1], [0, 1]);
+  const lightsOpacity = useTransform(lightsProgress, [0, 1], [0, 1]);
+  const cameraScale = useTransform(cameraProgress, [0, 1], [0, 1]);
+  const cameraOpacity = useTransform(cameraProgress, [0, 1], [0, 1]);
+  const actionScale = useTransform(actionProgress, [0, 1], [0, 1]);
+  const actionOpacity = useTransform(actionProgress, [0, 1], [0, 1]);
 
   return (
     <div id="movies" className="text-center">
@@ -11,9 +32,52 @@ export const Movies = ({ baseUrl, topFiveMovies }) => {
             <h2 className="top-movies sono-style">Top Movies</h2>
           </div>
         </div>
-          <div className="top-movies-subheader">
-            <span id="lights" className="thasadith-style">Lights,</span> <span id="camera" className="aboreto-style">Camera,</span> <span id="action" className="overpass-style">ACTION!!!</span>
+
+        <div id="lights-camera-action">
+          <div className="row">
+            <div className="col-2 offset-2">
+              <motion.div
+                className="thasadith-style"
+                ref={lightsRef}
+                style={{
+                  scale: lightsScale,
+                  opacity: lightsOpacity
+                }}
+              >
+                Lights,
+              </motion.div>
+            </div>
           </div>
+          <div className="row">
+            <div className="col-2 offset-5">
+            <motion.div
+              className="aboreto-style"
+              ref={cameraRef}
+              style={{
+                scale: cameraScale,
+                opacity: cameraOpacity,
+              }}
+            >
+              Camera,
+            </motion.div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-2 offset-8">
+            <motion.div
+                className="overpass-style"
+                ref={actionRef}
+                style={{
+                  scale: actionScale,
+                  opacity: actionOpacity
+                }}
+              >
+                ACTION!!!
+              </motion.div>
+            </div>
+          </div>
+        </div>
+
         <div id="row">
           {topFiveMovies.length > 0 && topFiveMovies[0]
             ? topFiveMovies.map((m, i) => (
--- src/components/PrivateWrappedSummary.jsx
+++ src/components/PrivateWrappedSummary.jsx
@@ -4,34 +4,28 @@ export const About = ({data, baseUrl, topGenre, topThreeMovies}) => {
   const topMovie = topThreeMovies[0];
 
   return (
-    <div id="about">
-      <div className="container">
-        <div className="row mb-5">
-          <div className="col-xs-12 col-md-6">
+      <div className="container" id="summary">
+        <div className="row" id="summary-row">
+          <div className="col-12 col-md-5 offset-md-1">
             <img src={`${baseUrl}${topMovie?.poster_path}`} className="summary-image" alt="N/A"/>
           </div>
-          <div className="col-xs-12 col-md-6">
-            <div className="about-text">
-
+          <div className="col-12 col-md-5">
+            <div className="summary-card">
               <h2 className="sono-style">Wrapped Summary</h2>
               <p>{data ? data.paragraph : "loading..."}</p>
               <h3 className="sono-style">Top 3 Movies:</h3>
-              <div className="list-style">
-                <div className="col-lg-6 col-sm-6 col-xs-12">
-                  <ul>
-                    {topThreeMovies.length > 0
-                      ? topThreeMovies?.map((movie, i) => (
-                          <li key={`${movie}-${i}`}>{movie?.title}</li>
-                        ))
-                      : "loading"}
-                  </ul>
-                </div>
-              </div>
+              <ul className="mb-4">
+                {topThreeMovies.length > 0
+                  ? topThreeMovies?.map((movie, i) => (
+                      <li key={`${movie}-${i}`}>{movie?.title}</li>
+                    ))
+                  : "loading"}
+              </ul>
               <h3 className="sono-style">Top Genre: {topGenre}</h3>
             </div>
           </div>
         </div>
       </div>
-    </div>
+
   );
 };
