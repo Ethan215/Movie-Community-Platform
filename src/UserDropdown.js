ada7fa80e9bafdf549d164e1e4d82b9f07159869
joshyeh123 <jayeh1111@gmail.com>
2024-03-12 17:37:10 -0700

    login button

--- src/UserDropdown.js
+++ src/UserDropdown.js
@@ -29,39 +29,52 @@ function UserDropdown() {
 return(
     <>
     {error && <Alert variant="danger">{error}</Alert>}
-<div style={{ position: 'absolute',top:'20px', right:'20px', zIndex: 1000 }}>
-          <Button onClick={toggleDropdown}>
-            {currentUser?.username}    {/*Display User name */}
-          </Button>
-          {showDropdown && (
-            <div style = {{
-              position: 'absolute',
-              top: '100%',
-              right: 0,
-              backgroundColor: 'white',
-              boxShadow: '0 8px 16px 0 rgba(0,0,0,0.2)',
-              padding: '12px 16px',
-              zIndex: 1001
 
-            }}
-            >
-              <Link to={`/user/${currentUser?.username}`} style={{  marginBottom: '10px', display: 'block' }}>My Account</Link>
-              <Link to={`/user/${currentUser?.username}/privateWrapped`} style={{  marginBottom: '10px', display: 'block'}}>Wrapped</Link>
-              <button
-              onClick={handleLogout}
-              style={{
+<div style={{ position: 'absolute', top: '20px', right: '20px', zIndex: 1000 }}>
+  {currentUser ? (
+    <>
+      <Button onClick={toggleDropdown}>
+        {currentUser.username} {/* Display User name */}
+      </Button>
+      {showDropdown && (
+        <div
+          style={{
+            position: 'absolute',
+            top: '100%',
+            right: 0,
+            backgroundColor: 'white',
+            boxShadow: '0 8px 16px 0 rgba(0,0,0,0.2)',
+            padding: '12px 16px',
+            zIndex: 1001,
+          }}
+        >
+          <Link to={`/user/${currentUser.username}`} style={{ marginBottom: '10px', display: 'block' }}>
+            My Account
+          </Link>
+          <Link to={`/user/${currentUser.username}/privateWrapped`} style={{ marginBottom: '10px', display: 'block' }}>
+            Wrapped
+          </Link>
+          <button
+            onClick={handleLogout}
+            style={{
               background: 'none',
               color: 'blue',
               cursor: 'pointer',
               textDecoration: 'underline',
               border: 'none',
               padding: 0,
-              font: 'inherit'
-            }}>Log Out
-            </button>
-          </div>
-          )}
-          </div>
+              font: 'inherit',
+            }}
+          >
+            Log Out
+          </button>
+        </div>
+      )}
+    </>
+  ) : (
+    <Button onClick={() => navigate('/')}>Login</Button> 
+  )}
+</div>
     </>
 );
 }
