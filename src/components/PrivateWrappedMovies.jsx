a162c6a9081edfaf5043834bd6fea4a7e49a021a
Kevin Nguyen <kevinvietnguyen2003@gmail.com>
2024-03-12 18:21:13 -0700

    Completed functionality of PrivateWrapped

--- src/components/PrivateWrappedMovies.jsx
+++ src/components/PrivateWrappedMovies.jsx
@@ -1,69 +1,6 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import { useParams } from 'react-router-dom';
-import { db } from '../contexts/firebase';
-import { collection, query, where, getDocs } from 'firebase/firestore';
+import React from 'react';
 
-export const Movies = (props) => {
-  const { username } = useParams();
-  const [topReviews, setTopReviews] = useState([]);
-  const [movies, setMovies] = useState([]);
-
-  const findTopFiveReviews = useCallback(reviewsData => {
-    if (reviewsData.length === 0) {
-      console.error('User has no reviews.');
-      return [];
-    }
-    // Sorting reviews in descending order based on rating
-    const sortedReviews = reviewsData.sort((a, b) => b.rating - a.rating);
-    // Getting top Five highest reviews
-    const topFiveReviews = sortedReviews.slice(0, 5);
-    // console.log(topFiveReviews)
-    return topFiveReviews;
-  }, []);
-
-  const fetchTopReviews = useCallback(async () => {
-    try {
-      const q = query(collection(db, 'reviews'), where('username', '==', username));
-      const querySnapshot = await getDocs(q);
-      const reviewsData = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-      const topFiveReviews = findTopFiveReviews(reviewsData);
-      setTopReviews(topFiveReviews);
-    } catch (error) {
-      console.error('Failed to fetch top movies:', error);
-    }
-  }, [username]);
-
-useEffect(() => {
-  const fetchTopReviewsAndUpdateMovies = async () => {
-    await fetchTopReviews(); // Make sure this updates `topReviews` state
-
-    // After topReviews are fetched and set, we proceed to fetch movie details
-    // Ensure fetchTopReviews function updates the topReviews state correctly
-    const moviePromises = topReviews.map((review) => {
-      const fetchUrl = `https://api.themoviedb.org/3/movie/${review.movie_id}?api_key=7fe3338624f016b56f11393910f1f293`;
-      return fetch(fetchUrl).then((response) => {
-        if (!response.ok) throw new Error('Movie not found');
-        return response.json();
-      }).catch(error => {
-        console.error("Failed to fetch movie details:", error);
-        return null; // Return null or some error indication for this movie
-      });
-    });
-
-    try {
-      // Wait for all movie detail fetches to complete
-      const movies = await Promise.all(moviePromises);
-      // Filter out any nulls/errors if necessary
-      setMovies(movies.filter(movie => movie !== null));
-    } catch (error) {
-      console.error("Failed to fetch movie details:", error);
-    }
-  };
-
-  // Calling the function to fetch reviews and then movies
-  fetchTopReviewsAndUpdateMovies();
-}, [username, fetchTopReviews, topReviews]); // topReviews dependency ensures it reruns when topReviews state updates
-
+export const Movies = ({ baseUrl, topFiveMovies }) => {
 
   return (
     <div id="movies" className="text-center">
@@ -75,12 +12,12 @@ useEffect(() => {
           </p>
         </div>
         <div id="row">
-          {movies.length > 0
-            ? movies.map((m, i) => (
+          {topFiveMovies.length > 0 && topFiveMovies[0]
+            ? topFiveMovies.map((m, i) => (
                 <div key={`${m.name}-${i}`} className="team">
                   <div className="thumbnail">
                     {" "}
-                    <img src={`${props.baseUrl}${m.poster_path}`} alt="Movie poster" className="team-img" />
+                    <img src={`${baseUrl}${m.poster_path}`} alt="Movie poster" className="team-img" />
                     <div className="caption">
                       <h4>{m.title}</h4>
                       <p>{m.release_date.slice(0,4)}</p>
--- src/components/PrivateWrappedSummary.jsx
+++ src/components/PrivateWrappedSummary.jsx
@@ -1,39 +1,33 @@
 import React from "react";
 
-export const About = (props) => {
+export const About = ({data, baseUrl, topGenre, topThreeMovies}) => {
+  const topMovie = topThreeMovies[0];
+
   return (
     <div id="about">
       <div className="container">
         <div className="row">
           <div className="col-xs-12 col-md-6">
-            {" "}
-            <img src="img/about.jpg" className="img-responsive" alt="" />{" "}
+            <img src={`${baseUrl}${topMovie?.poster_path}`} className="img-responsive" alt="" />
           </div>
           <div className="col-xs-12 col-md-6">
             <div className="about-text">
+
               <h2>User Summary</h2>
-              <p>{props.data ? props.data.paragraph : "loading..."}</p>
+              <p>{data ? data.paragraph : "loading..."}</p>
               <h3>User Info</h3>
               <div className="list-style">
                 <div className="col-lg-6 col-sm-6 col-xs-12">
                   <ul>
-                    {props.data
-                      ? props.data.Why.map((d, i) => (
-                          <li key={`${d}-${i}`}>{d}</li>
-                        ))
-                      : "loading"}
-                  </ul>
-                </div>
-                <div className="col-lg-6 col-sm-6 col-xs-12">
-                  <ul>
-                    {props.data
-                      ? props.data.Why2.map((d, i) => (
-                          <li key={`${d}-${i}`}> {d}</li>
+                    {topThreeMovies.length > 0
+                      ? topThreeMovies?.map((movie, i) => (
+                          <li key={`${movie}-${i}`}>{movie?.title}</li>
                         ))
                       : "loading"}
                   </ul>
                 </div>
               </div>
+              <h3>Top Genre: {topGenre}</h3>
             </div>
           </div>
         </div>
